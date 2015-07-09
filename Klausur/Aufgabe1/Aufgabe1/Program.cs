fd05f79e22af1b750537f95515bef2fdbcdc989b
Alexander Scheurer <mail@aspepex.net>
2015-07-09 16:12:06 +0200

    K - Aufgabe 1 a

--- Klausur/Aufgabe1/Aufgabe1/Program.cs
+++ Klausur/Aufgabe1/Aufgabe1/Program.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Aufgabe1
 {
-    class Vector3
+    class Vector<T>
     {
-        public double x, y, z;
+        public T x, y, z;
 
-        public static Vector3 Add(Vector3 v1, Vector3 v2)
+        public static Vector<T> Add(Vector<T> v1, Vector<T> v2)
         {
-            return new Vector3()
+            return new Vector<T>()
             {
                 x = v1.x + v2.x,
                 y = v1.y + v2.y,
@@ -20,9 +20,9 @@ namespace Aufgabe1
             };
         }
 
-        public static Vector3 Mul(double scalar, Vector3 v)
+        public static Vector<T> Mul(double scalar, Vector<T> v)
         {
-            return new Vector3()
+            return new Vector<T>()
             {
                 x = scalar*v.x,
                 y = scalar*v.y,
@@ -35,11 +35,13 @@ namespace Aufgabe1
     {
         static void Main(string[] args)
         {
-            Vector3 v1 = new Vector3() {x = 1.0, y = 2.0, z = 3.0};
-            Vector3 v2 = new Vector3() {x = 4.0, y = 5.0, z = 6.0};
+            Vector<double> v1 = new Vector<double>() {x = 1.0, y = 2.0, z = 3.0};
+            Vector<double> v2 = new Vector<double>() {x = 4.0, y = 5.0, z = 6.0};
 
-            Vector3 v3 = Vector3.Add(v1, v2);
-            Vector3 v4 = Vector3.Mul(2.0, v1);
+            Vector<double> v3 = Vector<double>.Add(v1, v2);
+            Vector<double> v4 = Vector<double>.Mul(2.0, v1);
+
+            Console.ReadKey();
         }
     }
 }
